60170fdfd0bc174efc05266faf622352b9f85cbd
gabuosh <zannoniarq@gmail.com>
2025-04-30 14:52:59 +0100

    Change in main class: added loop for menu and changed the order of questions for Add record

--- src/hospitalsorting/HospitalSorting.java
+++ src/hospitalsorting/HospitalSorting.java
@@ -8,42 +8,73 @@ public class HospitalSorting {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean running = true;
         
-        System.out.println("Enter employee name: ");
-        String name = sc.nextLine();
+        //add a loop for menu, so the user can interact multiple times
+        while (running){
+            System.out.println("\n Choose an option:");
+            for (int i = 0; i < MenuOption.values().length; i++){
+                System.out.println((i + 1) + ". " + MenuOption.values()[i]);
+            }
+            int choice = sc.nextInt();
+            sc.nextLine();
+            
+            switch(MenuOption.values()[choice - 1]){
+                
+                case Add_Record:
+                    System.out.println("\n Enter employee name: ");
+                    String name = sc.nextLine();
+                    
+                    //select employee role
+                    System.out.println("Select a role for the employee: ");
+                    for (int i = 0; i < EmployeeRole.values().length; i++){
+                        System.out.println((i + 1) + ". " + EmployeeRole.values()[i]);
+                    }
+                    int roleChoice = sc.nextInt();
+                    sc.nextLine();
+                    EmployeeRole employeerole = EmployeeRole.values()[roleChoice - 1];
         
-        //select type of manager
-        System.out.println("Select a manager type: ");
-        for (int i = 0; i < ManagerType.values().length; i++){
-            System.out.println((i + 1) + ". " + ManagerType.values()[i]);
+                    //select department
+                    System.out.println("Select the employee department: ");
+                    for (int i = 0; i < DepartmentType.values().length; i++){
+                        System.out.println((i + 1) + ". " + DepartmentType.values()[i]);
+                    }
+                    int departmentChoice = sc.nextInt();
+                    sc.nextLine();
+                    DepartmentType departmenttype = DepartmentType.values()[departmentChoice -1];
+                    
+                    //select type of manager
+                    System.out.println("Select a manager for the employee: ");
+                    for (int i = 0; i < ManagerType.values().length; i++){
+                        System.out.println((i + 1) + ". " + ManagerType.values()[i]);
+                    }
+                    int managerChoice = sc.nextInt();
+                    sc.nextLine();
+                    ManagerType managertype = ManagerType.values()[managerChoice - 1];
+        
+                    //create and store new employee
+                    Employee newEmployee = new Employee(name, managertype, departmenttype, employeerole);
+                    employees.add(newEmployee);
+        
+                    System.out.println("Employee added successfully: " + newEmployee + "\n");
+                    break;
+                    
+                case Sort:
+                    System.out.println("Sort not implemented yet...");
+                    break;
+                    
+                case Search:
+                    System.out.println("Sort not implemented yet...");
+                    break;
+                    
+                case Exit:
+                    running = false;
+                    System.out.println("Exiting program... ");
+                    break;
+            }
         }
-        int managerChoice = sc.nextInt();
-        sc.nextLine();
-        ManagerType managertype = ManagerType.values()[managerChoice - 1];
         
-        //select department
-        System.out.println("Select a department: ");
-        for (int i = 0; i < DepartmentType.values().length; i++){
-            System.out.println((i + 1) + ". " + DepartmentType.values()[i]);
-        }
-        int departmentChoice = sc.nextInt();
-        sc.nextLine();
-        DepartmentType departmenttype = DepartmentType.values()[departmentChoice -1];
-        
-        //select employee role
-        System.out.println("Select a role for the employee: ");
-        for (int i = 0; i < EmployeeRole.values().length; i++){
-            System.out.println((i + 1) + ". " + EmployeeRole.values()[i]);
-        }
-        int roleChoice = sc.nextInt();
-        sc.nextLine();
-        EmployeeRole employeerole = EmployeeRole.values()[roleChoice - 1];
-        
-        //create and store new employee
-        Employee newEmployee = new Employee(name, managertype, departmenttype, employeerole);
-        employees.add(newEmployee);
-        
-        System.out.println("Employee added successfully: " + newEmployee + "\n");
+
     }
     
 }
