07fd27d72640933047d03060a364391b9073c22c
gabuosh <zannoniarq@gmail.com>
2025-04-30 11:10:09 +0100

    Contructor to Employee class creating roles + getters and setters

--- src/hospitalsorting/Employee.java
+++ src/hospitalsorting/Employee.java
@@ -9,5 +9,35 @@ package hospitalsorting;
  * @author zzano
  */
 public class Employee {
+    private String name;
+    private ManagerType managertype;
+    private DepartmentType departmenttype;
+    private EmployeeRole employeerole;
     
+    //constructor
+    public Employee (String name, ManagerType managertype, DepartmentType departmenttype, EmployeeRole employeerole){
+        this.name = name;
+        this.managertype = managertype;
+        this.departmenttype = departmenttype;
+        this.employeerole = employeerole;
+    }
+    
+    //getters and setters
+    public String getName(){
+        return name;
+    }
+    public ManagerType getManagerType(){
+        return managertype;
+    }
+    public DepartmentType getDepartment(){
+        return departmenttype;
+    }
+    public EmployeeRole getEmployeeRole(){
+        return employeerole;
+    }
+    
+    public String toString(){
+        return name + " (" + employeerole + ") - " + departmenttype + " - Manager: " + managertype;
+    }
+        
 }
